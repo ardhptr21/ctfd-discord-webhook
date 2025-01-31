d2c66f37509370a64cb392d8f07d0ca51d743964
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-01-31 21:41:02 +0700

    add banner

--- webhook.py
+++ webhook.py
@@ -10,7 +10,6 @@ from .config import config
 
 import re
 from urllib.parse import quote
-from types import SimpleNamespace
 
 ordinal = lambda n: "%d%s" % (
     n,
@@ -21,10 +20,22 @@ sanreg = re.compile(
 )
 sanitize = lambda m: sanreg.sub(r"\1", m)
 
-topmessage = {
-    1: ":first_place: **First Blood!**:drop_of_blood: The battlefield just got claimed by **{user}** from team **{team}!** :rocket: They struck fast, solving the challenge **{challenge}** before anyone else could blink. Absolute legends in the making! :star2:",
-    2: ":second_place: **Wowza!** Big brains {user} from team **{team}** claimed the second solve **{challenge}** like pros! :fire: Staying cool :cold_face:",
-    3: ":third_place: **Respect!** **{user}** from team **{team}** snatched third solve **{challenge}** with grit and determination! :muscle: Never say die—what a brilliant! :clap: Keep the fire alive! :fire:",
+topConfig = {
+    1: {
+        "color": 0xff0000,
+        "title": "**[ :first_place: FIRST BLOOD :drop_of_blood: ]**",
+        "img_url": "https://i.ibb.co.com/rR9s18Hj/Banner-First-Blood.png"
+    },
+    2: {
+        "color": 0xc3baad,
+        "title": "**[ :second_place: SECOND BLOOD :drop_of_blood: ]**",
+        "img_url": "https://i.ibb.co.com/XfLZyZdJ/Banner-Second-Blood.png"
+    },
+    3: {
+        "color": 0xffaf3b,
+        "title": "**[ :third_place: THIRD BLOOD :drop_of_blood: ]**",
+        "img_url": "https://i.ibb.co.com/9Qq1Ybx/Banner-Third-Blood.png"
+    },
 }
 defaultmessage = (
     "Congratulations to team {team} for the {fsolves} solve on challenge {challenge}!"
@@ -68,7 +79,7 @@ def load(app):
                         solvers = solvers.filter(Solves.user.has(hidden=False))
                     num_solves = solvers.count()
 
-                    limit = app.config["DISCORD_WEBHOOK_LIMIT"]
+                    limit = int(app.config["DISCORD_WEBHOOK_LIMIT"])
                     if int(limit) > 0 and num_solves > int(limit):
                         return result
                     webhook = DiscordWebhook(url=app.config["DISCORD_WEBHOOK_URL"])
@@ -89,10 +100,18 @@ def load(app):
                         "category": sanitize(challenge.category),
                     }
 
-                    message = topmessage.get(num_solves, defaultmessage).format(
-                        **format_args
-                    )
-                    embed = DiscordEmbed(description=message)
+                    embed = DiscordEmbed()
+                    if num_solves > 3:
+                        message = defaultmessage.format(**format_args)
+                        embed.set_description(message)
+                    else:
+                        embed.add_embed_field(":game_die: Challenge", format_args["challenge"], inline=False)
+                        embed.add_embed_field(":flags: Team", format_args["team"], inline=False)
+                        embed.add_embed_field(":ninja: By", format_args["user"], inline=False)
+                        conf = topConfig.get(num_solves, {})
+                        embed.set_color(conf.get("color", 0x00ff00))
+                        embed.set_title(conf.get("title", ""))
+                        embed.set_image(url=conf.get("img_url", ""))
                     webhook.add_embed(embed)
                     webhook.execute()
             return result
